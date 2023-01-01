b4af5d4b99b418f3e55ca4907607e8e81fae626c
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 <wngns0812@gmail.com>
2023-01-01 15:26:44 +0900

    encapsulation

--- week9/chap09.go
+++ week9/chap09.go
@@ -1,4 +1,4 @@
-// call by reference
+// // call by reference
 package main
 
 import "fmt"
@@ -12,22 +12,6 @@ func (n *Number) Double() {
 func main() {
 	number := Number(4)
 	fmt.Println("원래 수:", number)
-A
-
-// // call by value
-// package main
-
-// import "fmt"
-
-// type Number int
-
-// func (n Numbner) Double(){
-// 	n = n * 2
-// }
-
-// func main(){
-// 	number := Number(4)
-// 	fmt.Println("원래 수:", number)
-// 	number.Double()
-// 	fmt.Println("두 배가된 수:" number)
-// }
+	number.Double()
+	fmt.Println(number)
+}
